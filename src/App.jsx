12b396972ed3451b418dd0b7906f97767115a054
Dark-Lord777 <207839762+Dark-Lord777@users.noreply.github.com>
2026-06-25 12:29:50 +0300

    add new str

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar' //navigation/ also do navigation bar
 import Projects from './pages/Projects.jsx'
+import Fortune from './pages/fortune.jsx'
 import Terms from './pages/Terms'
 import Privacy from './pages/Privacy'
 import Licenses from './pages/Licenses'
@@ -14,7 +15,7 @@ function App() {
         <Route path="/" element={<About />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects/>} />
-        <Route path="/projects/fortune" element={<Projects/>} />
+        <Route path="/projects/fortune" element={<Fortune/>} />
         <Route path="/projects/fortune/terms" element={<Terms />} />
         <Route path="/projects/fortune/privacy" element={<Privacy />} />
         <Route path="/projects/fortune/licenses" element={<Licenses />} />
